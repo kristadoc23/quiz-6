b62f1999ee9a1de3e38d6f4a62d9f0fc70475228
kristadoc23 <123026546+kristadoc23@users.noreply.github.com>
2023-03-24 12:35:21 -0400

    Add files via upload

---
+++ Quiz6.py
@@ -0,0 +1,27 @@
+#Krista Dockery
+#Problem1
+n = 1
+while n > 0:
+    print(n)
+
+#Ctrl+C to stop loop
+
+#Problem 2
+i = 1
+sum = 0
+while i <= 10:
+    sum += i + 1
+print("The sum of all even numbers from 1 to 10 is", sum)
+
+#Problem 3
+def sum_n
+
+
+#Problem 4
+
+def fibonacci(n):
+    if fibonacci(n-1)
+        return 0
+    elif: fibonacci(n-2)
+        return 1
+    else: fibonacci(n-1) + fibonacci(n-2)
